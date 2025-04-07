52021ff1f9ced7fbe97b2ca6c6621993a4665d2d
akbaroktaviadi89 <akbaroktaviadi89@gmail.com>
2025-04-07 16:34:00 +0700

    Adjust Field and ORM Model

--- app/Models/ProjectClient.php
+++ app/Models/ProjectClient.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProjectClient extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'occupation',
+        'avatar',
+        'logo',
+    ];
+
+    public function testimonial(){
+        return $this->hasMany(Testimonial::class, 'project_client_id');
+    }
 }
